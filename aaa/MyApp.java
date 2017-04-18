8ab9c4a9850eba4b2790b19654c333ace8e718f5
霍柏贺 <huobohe@huobohedeMacBook-Pro.local>
2017-04-18 18:45:38 +0800

    这是提交描述

---
+++ aaa/MyApp.java
@@ -0,0 +1,73 @@
+package lanou.com.fakebilibili.app;
+/**
+ * ██████齐天大圣 - 司帅████████
+ * <p>
+ * 　　 ◢████████████████◣
+ * 　　██　　　 ◥██◤　　　 ██
+ * 　◢███　　　　◥◤　　　  ██◣
+ * 　▊▎██◣　　　　　　　　 ◢█▊▊
+ * 　▊▎██◤　　●　 　●　   ◥█▊ ▊
+ * 　▊ ██　　　　　　　　　 █▊▊
+ * 　◥▇██　▊　　　　　　▊　 █▇◤
+ * 　　◥█　   ◥▆▄▄▆◤　    █◤　　　    ◢▇▇◣
+ * ◢████◥◥▆▅▄▂▂▂▂▂▂▂▄▅▅▆▆█████◣　   ▊◢　█
+ * █╳　　　　　　　　　　　　　　╳█　   ◥◤◢◤
+ * ◥█◣　　　˙　　　　　˙　　　◢█◤　　   ◢◤
+ * 　　▊　　　　　　　　　　　　▊　　　　█
+ * 　　▊　　　　　　　　　　　　▊　　　◢◤
+ * 　　▊　　　　　⊕　 　　　 　█▇▇▇◤
+ * ◢█▇▆▆▆▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▆▆▆▇█◣
+ * 　 ▊　▂　▊　　　　　　▊　▂　▊
+ **/
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+import com.hyphenate.easeui.controller.EaseUI;
+import com.uuzuche.lib_zxing.activity.ZXingLibrary;
+
+import cn.jpush.android.api.JPushInterface;
+import lanou.com.fakebilibili.activity.navtheme.AppConfig;
+
+/**
+ * Created by 司帅 on 17/3/11.
+ */
+
+public class MyApp extends Application {
+    private static Context context;
+    public static AppConfig appConfig;
+
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        appConfig = new AppConfig();
+        ZXingLibrary.initDisplayOpinion(this);
+        /**
+         * 即时通讯的初始化
+         */
+
+        EMOptions options = new EMOptions();
+        options.setAcceptInvitationAlways(true);
+        EaseUI.getInstance().init(this, options);
+        EMClient.getInstance().setDebugMode(true);
+
+        JPushInterface.init(this);
+        //可打印错误信息
+        JPushInterface.setDebugMode(true);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
