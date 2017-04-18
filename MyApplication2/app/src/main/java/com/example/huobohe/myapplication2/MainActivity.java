bf10cbfd17fcb10b45045154ba121064d30d2540
霍柏贺 <huobohe@huobohedeMacBook-Pro.local>
2017-04-18 19:21:50 +0800

    第二波测试

--- MyApplication2/app/src/main/java/com/example/huobohe/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/huobohe/myapplication2/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //又是一波测试
     }
 }
